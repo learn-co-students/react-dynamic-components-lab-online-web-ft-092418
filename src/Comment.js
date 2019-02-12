c658f45cecf5e697fd25faf2d5804819349c6009
github username <github email address>
2019-02-12 18:10:42 -0500

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,16 @@
 // add Comment component here
+import React, { Component } from 'react';
+
+export default class ColorBox extends Component {
+  
+    render() {
+      return (
+        <div className="comment" style={{opacity: 2}}>
+          {this.props.commentText}
+          {this.props.commentText}
+          {this.props.commentText}
+        </div>
+      )
+    }
+    
+}
